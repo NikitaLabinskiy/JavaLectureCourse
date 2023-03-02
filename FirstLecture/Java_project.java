db9883f835e4ad32412399347b5a3840928d1cca
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-03-02 22:39:34 +0300

    Первый код на Java

---
+++ FirstLecture/Java_project.java
@@ -0,0 +1,25 @@
+package FirstLecture;
+
+import java.util.Scanner;
+
+public class Java_project {
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[2J");
+        System.out.println("Hello world");
+        var x = 1223;
+        System.out.println(x);
+        FillAndPrintArray();
+    }
+
+    static void FillAndPrintArray() {
+        Scanner in = new Scanner(System.in);
+        int[] array = new int[5];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = in.nextInt();
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+}
